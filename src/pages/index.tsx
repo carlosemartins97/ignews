b1f8da9765ccaf72f4453c33c7182789a897b468
Carlos Martins <carlosemartins97@gmail.com>
2021-03-22 13:42:40 -0300

    feat: create global styles and all colors variables

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/Head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>
+        Hello <span>World</span>
+      </h1>
+    </>
+  )
+}
